b4e1bcc2684f9ec9deba4f7bd0a7f7ea13998e43
Kenta Shinohara <nocknocknock@gmail.com>
2023-08-27 03:21:47 +0900

    testコード修正

--- test/test-clip.sh
+++ test/test-clip.sh
@@ -9,14 +9,14 @@ docker build -f ./docker/Dockerfile-arm64 -t apex-clipper:local .
 cd ../test
 
 export target=clip
-export mode=match_clip_foreach
+export mode=match_clip
 export audio=0
 export expected_result=1
 
 ./test-action.sh
 
 export target=switch
-export mode=match_clip_foreach
+export mode=match_clip
 export audio=0
 export expected_result=2
 
