e48105a2b3b60ed01d6e14ccbd654ff8d276d7c9
Kenta Shinohara <nocknocknock@gmail.com>
2023-08-27 03:21:38 +0900

    ocr_in_progressを追加

--- test/test-action.sh
+++ test/test-action.sh
@@ -7,6 +7,7 @@ mode=${mode:-ocr}
 audio=${audio:-1}
 expected_result=${expected_result:-1}
 debug=${debug:-false}
+ocr_in_progress=${ocr_in_progress:-false}
 
 dummy_date="2023-08-01_10-00-00"
 
@@ -24,6 +25,16 @@ cp "../orig/${target}.mp4" orig/${dummy_date}.mp4
 
 if [ "$mode" == "match_clip" ]|| [ "$mode" == "match_clip_foreach" ] || [ "$mode" == "kill_clip" ]; then
     cp -r "../ocr/${target}" ./ocr/${dummy_date}
+
+    if "$ocr_in_progress"; then
+        for dir in match1 match2
+        do
+            if [ -d ./ocr/${dummy_date}/${dir} ]; then
+                rm ./ocr/${dummy_date}/${dir}/ocr_finished
+                touch ./ocr/${dummy_date}/${dir}/flg_in_progress_match
+            fi
+        done
+    fi
 fi
 
 cp ../apex_tracker.yml apex_tracker_tmp.yml
@@ -43,6 +54,7 @@ echo "run docker:"
 docker-compose -f apex_tracker_tmp.yml up
 
 if [ "$mode" == "ocr" ]; then
+
     echo "Check if kill scene is counted"
     result_num=$(cat ocr/${dummy_date}/cut_time_battle.csv | grep -c ,8, || true)
     if [ ${result_num} -ne ${expected_result} ]; then
@@ -51,7 +63,7 @@ if [ "$mode" == "ocr" ]; then
     fi
 fi
 
-if [ "$mode" == "match_clip" ]|| [ "$mode" == "match_clip_foreach" ]; then
+if [ "$mode" == "match_clip" ]; then
     echo "Check if merge file is created"
     result_num=$(find . -name "${dummy_date}_*_merge.mp4" | grep -c "mp4" || true)
     if [ ${result_num} -ne ${expected_result} ]; then
@@ -59,6 +71,26 @@ if [ "$mode" == "match_clip" ]|| [ "$mode" == "match_clip_foreach" ]; then
         exit 1
     fi
 fi
+
+if [ "$mode" == "match_clip_foreach" ]; then
+    if "$ocr_in_progress"; then
+        echo "Check file"
+        result_num=$(find . -name "*_flg_locked" | grep -c flg_locked || true)
+        if [ $result_num -ne 0 ]; then
+            echo "[ERROR] flg_locked file exists"
+            find . -name "*_flg_locked"
+            exit 1
+        fi
+    else
+        echo "Check if merge file is created"
+        result_num=$(find . -name "${dummy_date}_*_merge.mp4" | grep -c "mp4" || true)
+        if [ ${result_num} -ne ${expected_result} ]; then
+            echo "[ERROR] Num of merged file doesn't match. [$result_num != $expected_result]"
+            exit 1
+        fi
+    fi
+fi
+
 
 if [ "$mode" == "kill_clip" ]; then
     echo "Check if kill_clip is created"
--- test/test-clip-foreach.sh
+++ test/test-clip-foreach.sh
@@ -8,10 +8,31 @@ docker build -f ./docker/Dockerfile-arm64 -t apex-clipper:local .
 
 cd ../test
 
-export target=clip
+# # clip.mp4のクリップ作成
+
+# export target=clip
+# export mode=match_clip_foreach
+# export audio=0
+# export expected_result=1
+
+# ./test-action.sh
+
+# # switch.mp4のクリップ作成
+
+# export target=switch
+# export mode=match_clip_foreach
+# export audio=0
+# export expected_result=2
+
+# ./test-action.sh
+
+# OCR中フォルダへのクリップ作成
+
+export target=switch
 export mode=match_clip_foreach
 export audio=0
-export expected_result=1
+export expected_result=2
+export ocr_in_progress=true
 
 ./test-action.sh
 
