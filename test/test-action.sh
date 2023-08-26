c5f25c559ed43798b886a180cc90a75ff9274a22
Kenta Shinohara <nocknocknock@gmail.com>
2023-08-27 02:54:56 +0900

    Feature/action refactor (#32)
    * データファイル更新
    
    * テストスクリプトをgithub action用に修正
    
    * Action修正
    
    * ocrのvalidate追加
    
    * matrix拡充
    
    * testスクリプト修正
    
    * fix action
    
    * reduce ocr artifact

---
+++ test/test-action.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -eu
+
+target=${target:-clip}
+mode=${mode:-ocr}
+audio=${audio:-1}
+expected_result=${expected_result:-1}
+debug=${debug:-false}
+
+dummy_date="2023-08-01_10-00-00"
+
+# cd ../src
+
+# docker build -f ./docker/Dockerfile-arm64 -t apex-clipper:local .
+
+cd ../test
+
+rm -rf tmp
+mkdir -p tmp
+cd tmp
+mkdir -p orig out ocr work apex_kill_clip
+cp "../orig/${target}.mp4" orig/${dummy_date}.mp4
+
+if [ "$mode" == "match_clip" ]|| [ "$mode" == "match_clip_foreach" ] || [ "$mode" == "kill_clip" ]; then
+    cp -r "../ocr/${target}" ./ocr/${dummy_date}
+fi
+
+cp ../apex_tracker.yml apex_tracker_tmp.yml
+
+sed -i -e "s/<image-tag>/apex-clipper:local/" apex_tracker_tmp.yml
+sed -i -e "s/<mode>/${mode}/" apex_tracker_tmp.yml
+sed -i -e "s/<audio>/${audio}/" apex_tracker_tmp.yml
+sed -i -e "s/<debug>/${debug}/" apex_tracker_tmp.yml
+sed -i -e "s/<delay>/false/" apex_tracker_tmp.yml
+sed -i -e "s/<replica>/1/" apex_tracker_tmp.yml
+
+echo "docker-compose yaml:"
+cat apex_tracker_tmp.yml
+
+echo "run docker:"
+
+docker-compose -f apex_tracker_tmp.yml up
+
+if [ "$mode" == "ocr" ]; then
+    echo "Check if kill scene is counted"
+    result_num=$(cat ocr/${dummy_date}/cut_time_battle.csv | grep -c ,8, || true)
+    if [ ${result_num} -ne ${expected_result} ]; then
+        echo "[ERROR] Num of kill scene doesn't match. [$result_num != $expected_result]"
+        exit 1
+    fi
+fi
+
+if [ "$mode" == "match_clip" ]|| [ "$mode" == "match_clip_foreach" ]; then
+    echo "Check if merge file is created"
+    result_num=$(find . -name "${dummy_date}_*_merge.mp4" | grep -c "mp4" || true)
+    if [ ${result_num} -ne ${expected_result} ]; then
+        echo "[ERROR] Num of merged file doesn't match. [$result_num != $expected_result]"
+        exit 1
+    fi
+fi
+
+if [ "$mode" == "kill_clip" ]; then
+    echo "Check if kill_clip is created"
+    result_num=$(find ./apex_kill_clip/local -name "*.mp4" | grep -c "mp4" || true)
+    if [ ${result_num} -ne ${expected_result} ]; then
+        echo "[ERROR] Num of merged file doesn't match. [$result_num != $expected_result]"
+        exit 1
+    fi
+
+
+fi
+
+
+echo "run finished"
--- test/test-killclip.sh
+++ test/test-killclip.sh
@@ -8,15 +8,17 @@ docker build -f ./docker/Dockerfile-arm64 -t apex-clipper:local .
 
 cd ../test
 
-# # ocr
-# rm -rf ocr
-# mkdir -p ocr
+export target=kill_success
+export mode=kill_clip
+export audio=0
+export expected_result=1
 
-# # ffmpeg
-rm -rf out work apex_kill_clip
-mkdir -p out work apex_kill_clip
+./test-action.sh
 
-docker-compose -f apex_tracker_local_killclip.yml up
+export target=kill_failure
+export mode=kill_clip
+export audio=0
+export expected_result=0
 
-# ffmpeg -y -ss 10 -i ./src_movie/dir02/2022-12-30_23-38-24.mkv -t 10 -vcodec libx264 -acodec libmp3lame -vsync 1 -async 1000 -map 0:v:0 -map 0:a:1 -map 0:a:2 -map 0:a:3 work/test2.mp4
-# C^CReceived > 3 system signals, hard exiting=00:00:08.54 bitrate=5399.2kbits/s speed=0.0233x
+./test-action.sh
+
